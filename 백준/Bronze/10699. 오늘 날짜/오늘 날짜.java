3b3b303aeaeaa681ccab49b8e3181457ce7a80c0
Juhui <129067046+eggzuxi@users.noreply.github.com>
2025-06-24 20:24:12 +0900

    [Bronze V] Title: 오늘 날짜, Time: 96 ms, Memory: 13092 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,20 @@
+import java.time.LocalDate;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main {
+
+    public void Solution() throws Exception {
+
+        LocalDate now = LocalDate.now();
+        System.out.println(now);
+
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        new Main().Solution();
+
+    }
+}
