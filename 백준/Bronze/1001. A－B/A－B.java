8ec0d4a89eb62367dd44e8a91e9f136f8e1d28d5
Juhui <129067046+eggzuxi@users.noreply.github.com>
2025-06-24 20:50:05 +0900

    [Bronze V] Title: A-B, Time: 92 ms, Memory: 12880 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main {
+
+    public void Solution() throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a - b);
+
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        new Main().Solution();
+
+    }
+}
