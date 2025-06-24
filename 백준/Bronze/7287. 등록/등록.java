ce22cc92b4013a4ad12a65bf5c6d8d21724ff2a5
Juhui <129067046+eggzuxi@users.noreply.github.com>
2025-06-24 23:04:42 +0900

    [Bronze V] Title: 등록, Time: 60 ms, Memory: 11356 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main {
+
+    public void Solution() throws Exception {
+
+        System.out.println("8");
+        System.out.println("eggzuxi99");
+
+
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        new Main().Solution();
+
+    }
+}
