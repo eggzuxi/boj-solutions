0a5bea3a91a2652c6ee3e2da44fea2db6a167bd9
Juhui <129067046+eggzuxi@users.noreply.github.com>
2025-06-27 15:44:33 +0900

    [Bronze V] Title: A+B - 4, Time: 108 ms, Memory: 13124 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int sum = a + b;
+            System.out.println(sum);
+
+        }
+
+    }
+}
