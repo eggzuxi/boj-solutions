f0614be4bdfd3147c2af2d422430f1a00b362a2f
Juhui <129067046+eggzuxi@users.noreply.github.com>
2025-07-02 11:33:30 +0900

    [Bronze V] Title: 합, Time: 96 ms, Memory: 12848 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        System.out.println(sum);
+
+        sc.close();
+
+    }
+}
