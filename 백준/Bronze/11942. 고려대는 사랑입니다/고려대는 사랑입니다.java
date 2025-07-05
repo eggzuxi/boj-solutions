951c6d55d16907f6007519e2075302587d26a1cb
Juhui <129067046+eggzuxi@users.noreply.github.com>
2025-07-05 20:39:45 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 60 ms, Memory: 11204 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main {
+    
+    public static void main(String[] args) throws Exception {
+
+        System.out.print("고려대학교");
+
+    }
+}
