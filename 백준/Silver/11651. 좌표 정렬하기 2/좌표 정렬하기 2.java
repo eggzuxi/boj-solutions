a3275d618792abe67464a8b0038f6e1f75613dd5
Juhui <129067046+eggzuxi@users.noreply.github.com>
2025-07-15 15:31:11 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 1608 ms, Memory: 85724 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        int[][] loc = new int[t][2];
+
+        for (int i = 0; i < t; i++) {
+            String[] dot = br.readLine().split(" ");
+            loc[i][0] = Integer.parseInt(dot[0]);
+            loc[i][1] = Integer.parseInt(dot[1]);
+        }
+
+        br.close();
+
+        Arrays.sort(loc, (arr1, arr2) -> {
+
+            if (arr1[1] == arr2[1]) {
+                return Integer.compare(arr1[0], arr2[0]);
+            } else {
+                return Integer.compare(arr1[1], arr2[1]);
+            }
+
+        });
+
+        Arrays.stream(loc).forEach(x -> System.out.println(x[0] + " " + x[1]));
+
+    }
+}
